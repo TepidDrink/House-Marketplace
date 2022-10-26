88305042e94f4f765d0db0e491d974e308b71086
nicholaswsumner <niksum15@gmail.com>
2022-10-26 16:24:29 +0100

    Added listing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { ToastContainer } from "react-toastify"
-import NavBar from "./components/NavBar"
-import PrivateRoute from "./components/PrivateRoute"
-import Category from "./pages/Category"
-import CreateListing from "./pages/CreateListing";
-import Explore from "./pages/Explore"
-import ForgotPassword from "./pages/ForgotPassword"
-import Offers from "./pages/Offers"
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import NavBar from './components/NavBar'
+import PrivateRoute from './components/PrivateRoute'
+import Category from './pages/Category'
+import Contact from './pages/Contact'
+import CreateListing from './pages/CreateListing'
+import Explore from './pages/Explore'
+import ForgotPassword from './pages/ForgotPassword'
+import Listing from './pages/Listing'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
@@ -17,15 +19,17 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={ <Explore/> }/>
-          <Route path='/offers' element={ <Offers/> }/>
-          <Route path='/category/:categoryName' element={ <Category/> }/>
           <Route path='/profile' element={ <PrivateRoute/> }>
             <Route path='/profile' element={ <Profile/> }/>
           </Route>
           <Route path='/sign-in' element={ <SignIn/> }/>
           <Route path='/sign-up' element={ <SignUp/> }/>
           <Route path='/forgot-password' element={ <ForgotPassword/> }/>
+          <Route path='/offers' element={ <Offers/> }/>
+          <Route path='/category/:categoryName' element={ <Category/> }/>
+          <Route path='/category/:categoryName/:listingId' element={ <Listing/> }/>
           <Route path='/create-listing' element={ <CreateListing/> }/>
+          <Route path='/contact/:landlordId' element={ <Contact/> }/>
         </Routes>
         <NavBar/>
       </Router>
